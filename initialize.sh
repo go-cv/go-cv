eb3f4414a9bac15c7ab7223ef6905307574dc9e5
Bryan Joshua Pedini <bryan@pedini.dev>
2026-02-16 19:13:25 +0000

    Template updates (commit: 02db176)

--- initialize.sh
+++ initialize.sh
@@ -9,3 +9,8 @@ fi
 if [ ! -f "config.yaml" ]; then
   cp config.yaml.example config.yaml
 fi
+
+if [ ! -f "go.mod" ]; then
+  module_name=$(git remote get-url origin | sed 's|^https://||')
+  go mod init "$module_name"
+fi
